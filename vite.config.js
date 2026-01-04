0642ad21a0405a686dbb37dbce900160b2c83e61
Teachronen <rotem.ronen@gmail.com>
2026-01-04 04:28:11 -0500

    Completed CartItem component and Redux logic

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  server: {
+    port: 8888,
+    host: true
+  }
 })
